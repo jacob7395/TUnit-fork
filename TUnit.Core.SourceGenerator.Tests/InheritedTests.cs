994b41d19be9c2fc38a89741892f7a4809c5183c
Jacob <jacob7395@gmail.com>
2024-12-01 09:11:51 +0000

    tests: update test project type

---
+++ TUnit.Core.SourceGenerator.Tests/InheritedTests.cs
@@ -0,0 +1,10 @@
+namespace TUnit.Core.SourceGenerator.Tests;
+
+public class InheritedTests
+{
+    [Test]
+    public Task Test()
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ TUnit.Inherits.Tests/InheritsOtherProjectTests.cs
@@ -0,0 +1,9 @@
+using TUnit.Core;
+using TUnit.Core.SourceGenerator.Tests;
+
+namespace TUnit.Inherits.Tests;
+
+[InheritsTests]
+public class InheritsOtherProjectTests : InheritedTests
+{
+}
